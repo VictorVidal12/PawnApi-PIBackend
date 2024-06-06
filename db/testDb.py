c224d3065822155dcaf0813723c774bec3306ee7
VictorVidal12 <victormanuelvb12@gmail.com>
2024-06-05 21:09:57 -0500

    Add method get_peding_offer

--- db/testDb.py
+++ db/testDb.py
@@ -5,5 +5,5 @@ mydb = dbc.ConnectionDB()
 
 if __name__ == "__main__":
     id = 1
-    list = mydb.get_pending_sell_offerts_by_userid(1)
+    list = mydb.get_peding_sell_offer_by_userid(4)
     print(list)
